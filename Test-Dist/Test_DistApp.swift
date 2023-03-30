85debbf3cd449196e0ff053b20cfe250b0a89d94
Ian Applebaum <Ian.tyler@electricbluefish.com>
2023-03-30 11:53:15 -0400

    Initial Commit

---
+++ Test-Dist/Test_DistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_DistApp.swift
+//  Test-Dist
+//
+//  Created by Ian Applebaum on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_DistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
